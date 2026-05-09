9c89ea8213d60c4dd585226e9ca306e6acc6862f
Alex Morozov <lk4d4math@gmail.com>
2026-05-09 14:16:31 +0200

    Centralize download directory flow

--- suwayomi/manga/controller.lua
+++ suwayomi/manga/controller.lua
@@ -510,13 +510,10 @@ function Methods:confirmKeepNextUnreadChaptersDownloaded(limit)
     end
 
     local manga = self.current_chapter_context.manga
-    local download_directory = SuwayomiSettings:loadDownloadDirectory()
-    if not download_directory or download_directory == "" then
-        SuwayomiUI.showDirectoryChooser(function(path)
-            local saved_path = SuwayomiSettings:saveDownloadDirectory(path)
-            self:showMessage(T(_("Suwayomi download directory saved: %1"), saved_path))
+    local download_directory = self:getDownloadDirectoryOrChoose(function()
             self:confirmKeepNextUnreadChaptersDownloaded(limit)
-        end, self:getDownloadDirectoryChooserStartDir())
+    end)
+    if not download_directory then
         return 0
     end
 
