3a553b2bd0acffad40a6ac5bf551f0ab406bd543
Alexander Morozov <lk4d4math@gmail.com>
2026-05-22 15:22:14 +0200

    chore: release 1.0.3

--- _meta.lua
+++ _meta.lua
@@ -10,7 +10,7 @@ local _ = require("gettext")
 
 return {
     name = "suwayomi",
-    fullname = _("Suwayomi Client v1.0.2"),
+    fullname = _("Suwayomi Client v1.0.3"),
     description = _([[Suwayomi client for KOReader. Browse your Suwayomi server, manage source extensions, sync read state, and download chapters as local CBZ files.]]),
-    version = "1.0.2",
+    version = "1.0.3",
 }
